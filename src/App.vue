8ea3a27505f4537e668705669c1fefba0f0f2453
jackyscript <8930712+jackyscript@users.noreply.github.com>
2024-07-22 00:09:55 +0200

    Add navigation links

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,15 @@ import TrackingComponent from './components/time/TrackingComponent.vue'
 <template>
   <header role="banner">
     <h1>{{ $t('header.title') }}</h1>
+    <a href="#main">{{ $t('header.main') }}</a>
     <NavComponent></NavComponent>
   </header>
-  <main role="main" class="container-fluid">
+  <main id="main" role="main" class="container-fluid">
     <TrackingComponent></TrackingComponent>
   </main>
+  <footer>
+    <footer>
+      <a href="#top">{{ $t('footer.top') }}</a>
+    </footer>
+  </footer>
 </template>
