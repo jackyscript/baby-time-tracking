91bdddadf97255858a7db5cd1a54c81216e6110c
jackyscript <8930712+jackyscript@users.noreply.github.com>
2024-10-17 15:56:55 +0200

    Setup app locale

--- src/main.js
+++ src/main.js
@@ -7,15 +7,22 @@ import App from './App.vue'
 
 import en from './locales/en.json'
 import de from './locales/de.json'
+const DE_Language = 'de'
+const EN_Language = 'en'
+// supported languages
+const supportedLanguages = [DE_Language, EN_Language]
 // client language
 const clientLocale =
-  navigator.languages && navigator.languages.length > 0 ? navigator.languages[0] : 'en'
+  navigator.languages && navigator.languages.length > 0 ? navigator.languages[0] : EN_Language
 
+const appLocale = supportedLanguages.find((supportedLanguage) =>
+  clientLocale.includes(supportedLanguage)
+)
 // configure i18n
 const i18n = createI18n({
-  locale: clientLocale,
+  locale: appLocale ? appLocale : EN_Language,
   legacy: false,
-  fallbackLocale: 'en',
+  fallbackLocale: EN_Language,
   messages: { en, de }
 })
 const app = createApp(App)
