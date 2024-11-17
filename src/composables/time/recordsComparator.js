b371ef2d2247a4a009fbb9f6c04a27100080c2f2
jackyscript <8930712+jackyscript@users.noreply.github.com>
2024-11-17 09:55:46 +0100

    Refactor

--- src/composables/time/recordsComparator.js
+++ src/composables/time/recordsComparator.js
@@ -2,26 +2,27 @@ import { computed } from 'vue'
 
 export function useRecordsComparator(records) {
   return computed(() => {
-    const sortedEntries = Object.entries(records.value).sort(recordsComparator())
+    const sortedEntries = Object.entries(records.value).toSorted(recordsAscendingComparator)
     const sortedRecords = Object.fromEntries(sortedEntries)
     return sortedRecords
   })
 }
 
-function recordsComparator() {
-  return (recordBefore, recordAfter) => {
-    const attributesBefore = recordBefore[1]
-    const attributesAfter = recordAfter[1]
-    const recordBeforeDate = parseRecordDateValue(attributesBefore)
-    const recordAfterDate = parseRecordDateValue(attributesAfter)
-    return recordBeforeDate < recordAfterDate
-  }
+const recordsAscendingComparator = (recordBefore, recordAfter) => {
+  const attributesBefore = recordBefore[1]
+  const attributesAfter = recordAfter[1]
+
+  const recordBeforeDate = parseRecordDateValue(attributesBefore)
+  const recordAfterDate = parseRecordDateValue(attributesAfter)
+
+  return recordBeforeDate < recordAfterDate
 }
 
 function parseRecordDateValue(recordAttributes) {
   const recordDateValue = recordAttributes['entryDate'].entryValue
   const recordBeginTimeValue = recordAttributes['beginTime'].entryValue
   const dateTimeValue = `${recordDateValue}T${recordBeginTimeValue}` // Format: 2019-01-01T00:00:00
+
   if (recordBeginTimeValue) {
     return Date.parse(dateTimeValue)
   } else {
